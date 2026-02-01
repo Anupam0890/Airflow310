f5b4eba8522e51e6cbe80f76ade7699e8a80faa0
Anupam Pandit <iamanupampandit@gmail.com>
2026-02-01 11:25:21 +0000

    added task mapping

--- dags/dynamic_task_mapping_5.py
+++ dags/dynamic_task_mapping_5.py
@@ -8,7 +8,11 @@ def map_and_reduce():
         from random import randint
         return [i+1 for i in range(randint(1, 8))]
 
+    @task
+    def add_values(value: int, constant_val: int):
+        result = value + constant_val
+        return result
     
-    extra_value()
+    add_values.partial(constant_val=10).expand(value=extra_value())
 
 map_and_reduce()
