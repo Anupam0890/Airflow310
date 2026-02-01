831c0468c28285d78cc0229a7e2b0b214f3d120f
Anupam Pandit <iamanupampandit@gmail.com>
2026-02-01 11:56:38 +0000

    added filter

--- dags/dynamic_task_mapping_6.py
+++ dags/dynamic_task_mapping_6.py
@@ -1,7 +1,13 @@
 from airflow.sdk import dag, task
 
+def filter_file_extension(file: str) -> str:
+    if file.rsplit('.', 1)[1] == 'zip':
+        return None
+    
+    return file
+
 @dag
-def tranforming_and_filtering():
+def transforming_and_filtering():
 
     @task
     def extract_files() -> list[str]:
@@ -11,4 +17,13 @@ def tranforming_and_filtering():
     def add_path(file: str, path: str) -> str:
         return f"{path}/{file}"
 
-    add_path.partial(path="/tmp").expand(file=extract_files())
+    @task
+    def print_valid_files(files: list[str]) -> None:
+        for file in files:
+            print(file)
+
+    files_path =  add_path.partial(path="/tmp").expand(file=extract_files())
+    filtered_files = files_path.map(filter_file_extension)
+    print_valid_files(filtered_files)
+
+transforming_and_filtering()
